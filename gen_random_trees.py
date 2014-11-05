b67cd56e720d67277683ce5b932ed5f0234ef35c
PhyloStar <taraka@fripost.org>
2014-11-05 11:52:05 +0100

    Update gen_random_trees.py
    Added the branch length constraint.

--- gen_random_trees.py
+++ gen_random_trees.py
@@ -21,8 +21,24 @@ def gen_rand_tree(taxa_list):
         rnd.shuffle(taxa_list)
         
     taxa_list.append(";")    
-    return "".join(taxa_list)
-
+    return "".join(taxa_list)def gen_rand_tree_brlen(taxa_list):    
+    rnd.shuffle(taxa_list)
+    brlen_taxa_list = []
+    nbr = 2*len(taxa_list)-3
+    for taxa in taxa_list:
+        brlen_taxa_list.append(str(taxa)+":"+str(rnd.uniform(1,nbr)))
+    while(len(brlen_taxa_list)  > 1):
+        ulti_elem = str(brlen_taxa_list.pop())
+        penulti_elem = str(brlen_taxa_list.pop())   
+        if len(brlen_taxa_list) > 0:            
+            brlen_taxa_list.insert(0,"("+penulti_elem+","+ulti_elem+")"+":"+str(rnd.uniform(0,nbr)))
+        else:
+            brlen_taxa_list.insert(0,"("+penulti_elem+","+ulti_elem+")")
+        rnd.shuffle(brlen_taxa_list)
+        
+    brlen_taxa_list.append(";")
+    return "".join(brlen_taxa_list)
+    
 if __name__ == '__main__':    
     k = 10
     rep = 100   ##generates number of trees
